4e0659f7ba5048e273e1f59bf2638bf74aadab37
Wisley Rodrigues <103388410+Wisleey@users.noreply.github.com>
2023-04-24 20:53:55 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente extends Pessoa{
+    private String codigo;
+    private String profissao;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+}
