246e54de10588a25de7d20ab8a693d9df5017885
Wisley Rodrigues <103388410+Wisleey@users.noreply.github.com>
2023-04-24 20:52:34 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,48 @@
+public class Pessoa {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    private int dataNascimento;
+
+    public void cadastrar(){
+
+    }
+    public int obterIdade(){
+        int dataAtual = 2023;
+         int idade = dataAtual - dataNascimento;
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(int dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
